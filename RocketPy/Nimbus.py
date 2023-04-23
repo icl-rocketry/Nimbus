844030172bd58d4c3773798e6051488b41baa410
Usmaan Y <usmaan1901@gmail.com>
2023-04-23 21:19:21 +0100

    nothing important

--- RocketPy/Nimbus.py
+++ RocketPy/Nimbus.py
@@ -1,2 +1,10 @@
 # Nimbus is it a broomstick, I dont think so but it would've been so much more amazing 
 # Usmaan's going to suffer' - Kiran 2023
+from rocketpy import Environment, SolidMotor, Rocket, Flight
+
+# define environments
+Env = Environment(
+    railLength = 12, 
+    date = (2023, 1, 19, 12), 
+    latitude = -21.960641, longitude = -47.482122
+)
