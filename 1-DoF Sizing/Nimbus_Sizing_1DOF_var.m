64dc3fa3370f99d2400ba38e3c27f42ca1b0c681
usmaan19 <usmaan1901@gmail.com>
2023-09-26 20:14:54 +0100

    uh i broke something in the 1dof cus i did it quickly

--- 1-DoF Sizing/Nimbus_Sizing_1DOF_var.m
+++ 1-DoF Sizing/Nimbus_Sizing_1DOF_var.m
@@ -12,25 +12,29 @@ R = 287;            % think its called the gas constant right !? it's late and i
 
 % Engine Parameters
 OF = 3.5;                       % OF ratio
-m0_ox = 7;                      % oxidizer mass of rocket, kg
+m0_ox = 14;                      % oxidizer mass of rocket, kg
 m0_fuel = m0_ox/OF;             % fuel mass of rocket, kg
 m_propellant = m0_fuel + m0_ox; % total propellant mass, kg
 
-Isp = 250;   % Specific impulse of the rocket, s
+Isp = 220;   % Specific impulse of the rocket, s
 V_e = g*Isp; % exhaust velocity, m/s
-T = 15000;    % Thrust, N - we're gonna assume no regression for now
+T = 4500;    % Thrust, N - we're gonna assume no regression for now
 
 m_f_dot = (T/V_e)*(1/(OF+1));       % fuel mass flow rate
 m_ox_dot = (T/V_e)*(OF/(OF+1));     % oxidiser mass flow rate
 m_dot = m_f_dot + m_ox_dot;         % total mass flow rate
 
+t_burn = m_propellant/m_dot;
+
+T_impulse = T*t_burn;
+
 D_e = 60;              % nozzle exit diameter, mm - random guess but has negligible effect
 A_e = pi*(D_e/2000)^2; % nozzle exit area
 
 % Rocket Parameters
-m_dry = 35:1:55;  % dry mass of rocket, kg
-Cd = 0.6;        % drag coefficient (neglecting supersonic drag for now cus i dont think we are going this fast (but now we are ffs !!))
-D = 0.19;         % rocket diameter in m
+m_dry = 70;  % dry mass of rocket, kg
+Cd = 0.7;        % drag coefficient (neglecting supersonic drag for now cus i dont think we are going this fast (but now we are ffs !!))
+D = 0.20;         % rocket diameter in m
 A = (pi*D^2)/4;   % cross sectional area of rocket, m^2
 
 % Parachute Parameters
@@ -42,36 +46,36 @@ D_drogue = 1.22;                 % Drogue diameter, m
 A_drogue = (pi*D_drogue^2)/4;    % Drogue area, m^2
 
 %% iterate and plot
-apogee = zeros(length(m_dry),1);
-maxV = zeros(length(m_dry),1);
-
-for i = 1:length(m_dry)
-    DryMass = m_dry(i);
-    out = sim("Nimbus_Sizing_1DOF.slx");
-    apogee(i) = max(out.altitude);
-    maxV(i) = max(out.velocity);
-end
-
-apogeeFig = figure;
-hold on
-plot(m_dry,apogee)
-yline(3000,LineWidth=2)
-hold off
-xlabel('Dry Mass, kg')
-ylabel('Predicted Apogee, m')
-nicePlot(apogeeFig)
-
-maxVFig = figure;
-plot(m_dry,maxV)
-xlabel('Dry Mass, kg')
-ylabel('Max Velocity, ms$^{-1}$')
-nicePlot(maxVFig)
-
-machFig = figure;
-hold on
-plot(m_dry,maxV./atmos(3000,2))
-yline(1,LineWidth=2)
-hold off
-xlabel('Dry Mass, kg')
-ylabel('Mach Number')
-nicePlot(machFig)
+% apogee = zeros(length(m_dry),1);
+% maxV = zeros(length(m_dry),1);
+% 
+% for i = 1:length(m_dry)
+%     DryMass = m_dry(i);
+%     out = sim("Nimbus_Sizing_1DOF.slx");
+%     apogee(i) = max(out.altitude);
+%     maxV(i) = max(out.velocity);
+% end
+% 
+% apogeeFig = figure;
+% hold on
+% plot(m_dry,apogee)
+% yline(3000,LineWidth=2)
+% hold off
+% xlabel('Dry Mass, kg')
+% ylabel('Predicted Apogee, m')
+% nicePlot(apogeeFig)
+% 
+% maxVFig = figure;
+% plot(m_dry,maxV)
+% xlabel('Dry Mass, kg')
+% ylabel('Max Velocity, ms$^{-1}$')
+% nicePlot(maxVFig)
+% 
+% machFig = figure;
+% hold on
+% plot(m_dry,maxV./atmos(3000,2))
+% yline(1,LineWidth=2)
+% hold off
+% xlabel('Dry Mass, kg')
+% ylabel('Mach Number')
+% nicePlot(machFig)
